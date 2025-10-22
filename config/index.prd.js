c8f33aef4bb53ffb82f1b4f7acac8510243383a0
omid kianfar <kianfar0991@gmail.com>
2025-10-22 10:49:39 +0330

    fix firebase config

--- config/index.prd.js
+++ config/index.prd.js
@@ -1,6 +1,9 @@
-
-
 const config = {
-	
+	apiKey: 'AIzaSyBX_fsqBYSET-fpGrH1Ah3FPVMazNswgTU',
+	authDomain: 'infuuse-57e96.firebaseapp.com',
+	projectId: 'infuuse-57e96',
+	storageBucket: 'infuuse-57e96.firebasestorage.app',
+	messagingSenderId: '659422342639',
+	appId: '1:659422342639:web:0c478250b94207a7dfda1a',
 };
 module.exports = config;
