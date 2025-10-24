65a7f6ee0b08629d20a0d5a6a9277bc672508614
omid kianfar <kianfar0991@gmail.com>
2025-10-24 08:00:13 +0330

    fix layout

--- src/layout/main-layout.tsx
+++ src/layout/main-layout.tsx
@@ -3,11 +3,7 @@ import { Stack } from '@mui/material';
 import React, { PropsWithChildren } from 'react';
 import { CustomLayout, CustomMainLayoutChildren, Main } from './styles';
 import FirstSidebar from '@/components/molecules/sidebars/first-sidebar';
-import SettingSidebar from '@/components/molecules/sidebars/second-sidebar/setting';
-import SecondSidebar from '@/components/molecules/sidebars/second-sidebar';
 import MainHeader from '@/components/molecules/header/main-header';
-import { useSnapshot } from 'valtio';
-import settingStore from '@/store/setting.store';
 import SidebarSecond from './sidebar';
 
 const MainLayout = ({ children }: PropsWithChildren) => {
