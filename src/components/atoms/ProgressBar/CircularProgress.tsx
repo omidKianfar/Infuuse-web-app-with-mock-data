debfb005ea13b38a1614e9ec73006125f80dab1a
omid kianfar <kianfar0991@gmail.com>
2025-10-24 16:57:42 +0330

    fix LoadingProgress

--- src/components/atoms/ProgressBar/CircularProgress.tsx
+++ src/components/atoms/ProgressBar/CircularProgress.tsx
@@ -3,11 +3,11 @@ import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 
 interface Props {
-	size?: number;
-	color?: any;
+	size: number;
+	color: any;
 }
 
-export default function LoadingProgress({ size, color }: Props) {
+export default function LoadingProgress({ size, color }: Partial<Props>) {
 	return (
 		<Box sx={{ display: 'flex' }}>
 			<CircularProgress size={size} sx={{ color: color ? color : '#fff' }} />
